36b12f0c538fe0423adf0a8951be293d9b067676
blaser69 <quachgiabao2341@gmail.com>
2026-04-14 19:15:43 +0700

    Add AudioPlayer, POI, Location

---
+++ user/src/types/index.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export interface POI {
+    id: string;
+    name: string;
+    specialty: string;
+    hours: string;
+    rating: number;
+    lat: number;
+    lng: number;
+    audioUrl: string;
+    description: string;
+    image: string;
+}
+
+export interface Location {
+    lat: number;
+    lng: number;
+}
+
+export type TabType = 'map' | 'list' | 'tracking';
